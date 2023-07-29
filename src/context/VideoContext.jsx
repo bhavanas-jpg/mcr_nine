2a060bbee9cf5b366a18f9901187350ece72e57c
pavan6cs <bhavanas904@gmail.com>
2023-07-29 15:10:09 +0530

    feat: explore

--- src/context/VideoContext.jsx
+++ src/context/VideoContext.jsx
@@ -1,17 +1,20 @@
 import { createContext, useContext, useState } from "react";
 import {categories} from "../data/categories.js"
+import { videos } from "../data/videos.js";
 
 
 export const VideoContext = createContext(null);
 
 export const VideoProvider =({children}) =>{
  const [categoriesData, setCategoriesData] = useState(categories);
+ const [videosData, setVideosData]= useState(videos)
 
 
 
 return(
     <VideoContext.Provider value={{
-        categoriesData
+        categoriesData,
+        videosData
     }}>
         {children}
     </VideoContext.Provider>
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { useVideoContext } from "../context/VideoContext";
+import { MdOutlineWatchLater } from "react-icons/md";
 
 const Explore = () => {
+  const { videosData} = useVideoContext();
+
+  console.log(videosData);
   return (
     <main className="container">
       <div className="content">
@@ -9,7 +14,33 @@ const Explore = () => {
           <Navbar />
         </aside>
         <section className="main__sec">
-          <div>Explore</div>
+          <h3>Explore</h3>
+          <div>
+            <input 
+            className="search__input"
+            type="search" 
+            placeholder="search by title"
+            />
+          </div>
+          <div className="card">
+          {
+           videosData.map(video=>(
+            <div className="card__content"> 
+              <span className="watchlater__icon">
+                <MdOutlineWatchLater />
+              </span>
+              <img src={video.thumbnail} alt=""
+              width="100%" height="auto"
+              />
+              <h5>{video.title}</h5>
+              <h5>{video.category }</h5>
+              <span
+              className="card__text"
+              >{video.views} Views | {video.creator}</span>
+            </div>
+           ))
+          }
+          </div>
         </section>
       </div>
     </main>
